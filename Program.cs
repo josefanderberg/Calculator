544cd6c41006a5282f4c873c7b92d4078ced1c74
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-08 18:58:47 +0200

    First upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Skriv ett tal:");
+int tal1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Skriv ett till tal:");
+int tal2 = Convert.ToInt32(Console.ReadLine());
+
+int summa = tal1 + tal2;
+Console.WriteLine("Summan är: " + summa);
