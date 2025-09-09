294c3fbfa24b9e046196495559b93767eae59518
Josef Anderberg <josef.anderberg@gmail.com>
2025-09-09 16:02:06 +0200

    Latest update

--- Program.cs
+++ Program.cs
@@ -4,27 +4,34 @@ class Calculator
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Välj räknesätt: \n + \n - \n * \n /");
-        string calc = Console.ReadLine()!;
-        Console.WriteLine("Skriv ditt första tal");
-        int x = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine("Skriv ditt andra tal");
-        int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Välj räknesätt: \n + \n - \n * \n /");
+            string calc = Console.ReadLine()!;
+            if (calc == "+" || calc == "-" || calc == "*" || calc == "/")
+            {
+            }
+            else
+            {
+                Console.WriteLine("Skrev du verkligen +, -, * eller /");
+            }
 
-        int result = 0;
+            Console.WriteLine("Skriv ditt första tal");
+            int x = Convert.ToInt32(Console.ReadLine());
 
-        if (calc == "+")
-        { result = x + y; }
-        else if (calc == "-")
-        { result = x - y; }
-        else if (calc == "*" || calc == "x")
-        { result = x * y; }
-        else if (calc == "/")
-        { result = x / y; }
-        else
-        {
-            Console.WriteLine("Skrev du verkligen +, -, * eller ");
+            Console.WriteLine("Skriv ditt andra tal");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int result = 0;
+
+            if (calc == "+")
+            { result = x + y; }
+            else if (calc == "-")
+            { result = x - y; }
+            else if (calc == "*")
+            { result = x * y; }
+            else if (calc == "/")
+            { result = x / y;}
+            else
+            {}
+            Console.WriteLine("Summan är " + result);
         }
-        Console.WriteLine("Summan är " + result);
-    }
 }
